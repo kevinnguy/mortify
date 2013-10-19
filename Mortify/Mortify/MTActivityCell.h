4f583eac391a9bfe86d2378d63c396a49d10ec8f
Kevin Nguy <kevnguy@gmail.com>
2013-10-19 10:41:52 -0700

    Added MTActivityCell

---
+++ Mortify/Mortify/MTActivityCell.h
@@ -0,0 +1,15 @@
+//
+//  MTActivityCell.h
+//  Mortify
+//
+//  Created by America on 10/19/13.
+//  Copyright (c) 2013 Kevin Nguy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTActivityCell : UITableViewCell
+@property (nonatomic, strong) UILabel *microMortLabel;
+@property (nonatomic, strong) UILabel *activityLabel;
+
+@end
---
+++ Mortify/Mortify/MTActivityCell.m
@@ -0,0 +1,45 @@
+//
+//  MTActivityCell.m
+//  Mortify
+//
+//  Created by America on 10/19/13.
+//  Copyright (c) 2013 Kevin Nguy. All rights reserved.
+//
+
+#import "MTActivityCell.h"
+
+#import <QuartzCore/QuartzCore.h>
+
+@implementation MTActivityCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+        self.microMortLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 40, 40)];
+        self.microMortLabel.font = [UIFont systemFontOfSize:26.0f];
+        self.microMortLabel.textAlignment = NSTextAlignmentCenter;
+        self.microMortLabel.textColor = [UIColor purpleColor];
+        self.microMortLabel.layer.borderColor = self.microMortLabel.textColor.CGColor;
+        self.microMortLabel.layer.borderWidth = 1.0f;
+        self.microMortLabel.layer.cornerRadius = CGRectGetWidth(self.microMortLabel.frame) / 2;
+        
+        self.activityLabel = [[UILabel alloc] initWithFrame:CGRectMake(10 + CGRectGetWidth(self.microMortLabel.frame) + 10, 10, CGRectGetWidth(self.contentView.frame) - 10 - CGRectGetWidth(self.microMortLabel.frame) - 10, 40)];
+        self.activityLabel.font = [UIFont systemFontOfSize:26.0f];
+        self.activityLabel.textColor = [UIColor blueColor];
+        
+        [self.contentView addSubview:self.microMortLabel];
+        [self.contentView addSubview:self.activityLabel];
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    
+}
+
+@end
